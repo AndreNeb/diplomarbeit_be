6db0aeecfa6ce7509d34a8a9fdbaeb68d47d8224
vikto <viktoria.wuercher06@htl-salzburg.ac.at>
2024-10-10 15:34:35 +0200

    commit 1

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-from flask import Flask, render_template, request, jsonify
-import Diplomarbeit_BE
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Startseite mit Button
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route, die aufgerufen wird, wenn der Button geklickt wird
+@app.route('/button-click')
+def button_click():
+    return "Der Button wurde geklickt!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
